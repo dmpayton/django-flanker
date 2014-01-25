d7d00a093e9aa7841a4f17cd4c41e0c755f57f57
dmpayton <derek.payton@gmail.com>
2014-01-25 03:06:56 -0800

    Update descriptions before pushing to PyPI.

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 django-flanker
 ==============
 
-|travis-ci|_
+|travis-ci|_ |coveralls|_ |downloads|_
+
+Django-flanker provides a drop-in replacement EmailFields for Django forms and
+models that uses Mailgun's `flanker <https://github.com/mailgun/flanker>`_
+library for more extensive validation.
 
 .. |travis-ci| image:: https://secure.travis-ci.org/dmpayton/django-flanker.png
 .. _travis-ci: http://travis-ci.org/dmpayton/django-flanker
 
+.. |coveralls| image:: https://coveralls.io/repos/dmpayton/django-flanker/badge.png
+.. _coveralls: https://coveralls.io/r/dmpayton/django-flanker
+
+.. |downloads| image:: https://pypip.in/d/django-flanker/badge.png
+.. _downloads: https://pypi.python.org/pypi/django-flanker/
 
 """
 
