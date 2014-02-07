602b0100043f46ee9d821d20ccb23ea969b4fbab
dmpayton <derek.payton@gmail.com>
2014-02-07 14:38:34 -0800

    Add proper error messages so that suggestions are displayed in Django 1.5 and below.

--- django_flanker/forms.py
+++ django_flanker/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from . import validators
 
 
 class EmailField(forms.EmailField):
+    default_error_messages = {
+        'invalid': _('Enter a valid email address.'),
+        'invalid_typo': _('Enter a valid email address. (Did you mean: "%(suggestion)s"?)')
+    }
     default_validators = [validators.flanker_validator]
--- django_flanker/validators.py
+++ django_flanker/validators.py
@@ -1,11 +1,15 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import EmailValidator
-from django.utils.translation import string_concat, ugettext_lazy as _
+from django.core.validators import validate_email
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
 from flanker.addresslib import address, validate
 
 
-class FlankerValidator(EmailValidator):
-    suggest_fragment = _('(Did you mean: "%(suggestion)s"?)')
+class FlankerValidator(object):
+    code_invalid = 'invalid'
+    code_typo = 'invalid_typo'
+    message_invalid = _('Enter a valid email address.')
+    message_typo = _('Enter a valid email address. (Did you mean: "%(suggestion)s"?)')
 
     def __init__(self, suggest_fragment=None, **kwargs):
         super(FlankerValidator, self).__init__(**kwargs)
@@ -13,16 +17,15 @@ class FlankerValidator(EmailValidator):
             self.suggest_fragment = suggest_fragment
 
     def __call__(self, value):
-        super(FlankerValidator, self).__call__(value)
+        validate_email(value)
+        value = force_text(value)
 
         if value and address.validate_address(value) is None:
             suggestion = validate.suggest_alternate(value)
             if suggestion:
-                message = string_concat(self.message, ' ', self.suggest_fragment)
-                raise ValidationError(message, code=self.code, params={
-                    'suggestion': suggestion
-                })
-            raise ValidationError(self.message, code=self.code)
+                raise ValidationError(self.message_typo, code=self.code_typo,
+                    params={'suggestion': suggestion})
+            raise ValidationError(self.message_invalid, code=self.code_invalid)
 
 
 flanker_validator = FlankerValidator()
